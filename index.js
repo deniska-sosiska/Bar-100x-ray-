bf732b91efc3b6b5d52ba3d6ddc1bca03ca8fc52
deniska-sosiska <denchikarabik@gmail.com>
2020-07-03 20:11:50 +0300

    Создал сервер получается

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const http = require('http')
+// const http = require('http')
+
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+   res.sendfile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+  console.log("hi girls")
+})
