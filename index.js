91a67205177fc0f0149617c962783f2e6def129c
deniska-sosiska <denchikarabik@gmail.com>
2020-07-07 18:39:29 +0300

    На главной странице сделал: выбор комнаты

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io').listen(server)
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/site'));
 
 
-app.get('/', function(request, response) {
-  response.sendfile(path.join(__dirname + '/site/index.html'))
-})
+
+// app.get('/', function(request, response) {
+//   response.sendfile(path.join(__dirname + '/site/index.html'));
+// })
 
 
-server.listen(9999)
+server.listen(9999);
