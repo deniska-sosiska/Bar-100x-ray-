d11c23e61c02104dfe68bc9d468c74f271f6925a
deniska <denchikarabik@gmail.com>
2021-04-17 12:53:35 +0300

    complete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Подключаю модули, создаю сервер
 const createCards = require('./public/js/createCards.js'); // Мой модуль, который создает новые карточки
-const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -9,32 +8,34 @@ const io = require('socket.io').listen(server);
 app.set('view engine', 'ejs');
 app.use('/public', express.static('./public'));
 
+let updateDataCards = createCards()
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    countUsers: Number(updateDataCards.users)
+  });
 })
 
-app.get('/room:id', function(req, res) {
+// app.post('/update', (req, res) => {
+//   updateDataCards = createCards()
+//   res.render('backpage')
+//   res.end()
+// }) 
+// ↑↑↑ crush server ↑↑↑
+
+app.get('/room:id', (req, res) => {
   myNumber = req.params.id;
-  res.render('room', {roomID: myNumber, catastrophe: createCards[0], attribute: createCards[myNumber]});
+  res.render('room', {
+    roomID: Number(myNumber),
+    catastrophe: updateDataCards.cards[0],
+    attribute: updateDataCards.cards[myNumber]
+  });
 })
 
 //"Слушаю" сервер
-server.listen(9999);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+server.listen(9999, () => {
+  console.log("localhost:9999")
+});
 
 
 //========================================================
--- public/js/createCards.js
+++ public/js/createCards.js
@@ -2,51 +2,58 @@ const myFunc = require('./myFunc') // Подключаю свои функции
 const Card = require('./myClass') // Подключаю свои функции и класс
 const random = require('random');
 
-//var
-let users = 9;
-let cards = [];
+function getData() {
+  //var
+  let users = 9;
+  let cards = [];
 
-// int main() {
-for (let i = 0; i < users; i++) {
-  // получаю атрибуты
-  let forProfession = myFunc.funcToGetStr('Профессии');
-  let profession = myFunc.getAttribute(forProfession);
-  let forHealth = myFunc.funcToGetStr('Болезни');
-  let health = myFunc.getAttribute(forHealth);
-  let forBaggage = myFunc.funcToGetStr('Багаж');
-  let baggage = myFunc.getAttribute(forBaggage);
-  let forHobby = myFunc.funcToGetStr('Хобби');
-  let hobby = myFunc.getAttribute(forHobby);
-  let forPhobias = myFunc.funcToGetStr('Фобии');
-  let phobias = myFunc.getAttribute(forPhobias);
-  let forHumanQuality = myFunc.funcToGetStr('Качество человека');
-  let humanQuality = myFunc.getAttribute(forHumanQuality);
+  // int main() {
+  for (let i = 0; i < users; i++) {
+    // получаю атрибуты
+    let forProfession = myFunc.funcToGetStr('Профессии');
+    let profession = myFunc.getAttribute(forProfession);
+    let forHealth = myFunc.funcToGetStr('Болезни');
+    let health = myFunc.getAttribute(forHealth);
+    let forBaggage = myFunc.funcToGetStr('Багаж');
+    let baggage = myFunc.getAttribute(forBaggage);
+    let forHobby = myFunc.funcToGetStr('Хобби');
+    let hobby = myFunc.getAttribute(forHobby);
+    let forPhobias = myFunc.funcToGetStr('Фобии');
+    let phobias = myFunc.getAttribute(forPhobias);
+    let forHumanQuality = myFunc.funcToGetStr('Качество человека');
+    let humanQuality = myFunc.getAttribute(forHumanQuality);
 
-  // Получаю доп. атрибуты
-  if (random.int(0, 3) == 3) {
-    forAdditionalInfo = myFunc.funcToGetStr('Дополнительная информация');
-    additionalInfo = myFunc.getAttribute(forAdditionalInfo);
-  } else {  additionalInfo = 'Нету';  }
-  if (random.int(0, 3) == 3) {
-    forAction = myFunc.funcToGetStr('Карты действия');
-    action = myFunc.getAttribute(forAction);
-  } else {  action = 'Нету';  }
+    // Получаю доп. атрибуты
+    if (random.int(0, 3) == 3) {
+      forAdditionalInfo = myFunc.funcToGetStr('Дополнительная информация');
+      additionalInfo = myFunc.getAttribute(forAdditionalInfo);
+    } else {  additionalInfo = 'Нету';  }
+    if (random.int(0, 3) == 3) {
+      forAction = myFunc.funcToGetStr('Карты действия');
+      action = myFunc.getAttribute(forAction);
+    } else {  action = 'Нету';  }
 
-  // Получаю Биологичесткую характеристику
-  let age = random.int(18, 68);
-  if (random.int(0, 1) == 1) {  sex = 'Мужчина';  }
-  else {  sex = 'Женщина';  }
-  if (random.int(0, 3) == 3) {  biologicalCharacterization = [sex, age,'чайлдфри']; }
-  else {  biologicalCharacterization = [sex, age];  }
+    // Получаю Биологичесткую характеристику
+    let age = random.int(18, 68);
+    if (random.int(0, 1) == 1) {  sex = 'Мужчина';  }
+    else {  sex = 'Женщина';  }
+    if (random.int(0, 3) == 3) {  biologicalCharacterization = [sex, age,'чайлдфри']; }
+    else {  biologicalCharacterization = [sex, age];  }
 
-  // создаю/перезаписываю класс, и добавляю его в массив
-  let card = new Card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality, additionalInfo, action);
-  cards.push(card);
+    // создаю/перезаписываю класс, и добавляю его в массив
+    let card = new Card(profession, health, biologicalCharacterization, baggage, hobby, phobias, humanQuality, additionalInfo, action);
+    cards.push(card);
+  }
+
+  let forCatastrophe = myFunc.funcToGetStr('Катастрофы');
+  let catastrophe = myFunc.createCatastrophe(forCatastrophe);
+  cards.unshift(catastrophe);
+  // console.log(cards);
+
+  return {
+    cards,
+    users
+  }
 }
-
-let forCatastrophe = myFunc.funcToGetStr('Катастрофы');
-let catastrophe = myFunc.createCatastrophe(forCatastrophe);
-cards.unshift(catastrophe);
-// console.log(cards);
-module.exports = cards;
+module.exports = getData;
 //}
